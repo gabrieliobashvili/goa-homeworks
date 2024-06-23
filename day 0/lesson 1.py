e31fb5b15a7f30c907949be32e57a202f75ac5bb
gabrieliobashvili <iobashviligabrieli@gmail.com>
2024-06-23 22:09:35 +0400

    second commit

---
+++ day 0/lesson 1.py
@@ -0,0 +1,38 @@
+name = "gabriel"
+
+
+
+surname = "iobashvili"
+
+
+
+
+
+print(name)
+age = 9
+height = 141.0
+
+
+knows_programing = True
+is_ugly = False
+
+isugly = False
+
+
+#print(name + " "  + surname)
+
+
+#
+
+#print(type(age))
+#nt(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+
+print(name + " " + str(age))
+print(name + " "  +  " " +  surname + " " + str(age))
+
+
+
+
