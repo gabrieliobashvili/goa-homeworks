5c80178367edfd469326181c5519dd7185cd1750
gabrieliobashvili <iobashviligabrieli@gmail.com>
2024-07-02 18:58:12 +0400

    second commit

---
+++ day 2/classwork/level 3.py/level 3.py
@@ -0,0 +1,19 @@
+rur = 9
+
+print(rur + 10  )
+
+rare = 4
+
+
+print(rare * 5)
+
+goa = " GOA "
+print(goa * 1000)
+
+monkey = 30
+monkey_banana = 1
+
+print(monkey - monkey_banana)
+
+
+
---
+++ day 2/homework/level 3.py
@@ -0,0 +1,36 @@
+rur = 9
+
+print(rur + 10  )
+
+rare = 4
+
+
+print(rare * 5)
+
+goa = " GOA "
+print(goa * 1000)
+
+monkey = 30
+monkey_banana = 1
+
+print(monkey - monkey_banana)
+
+price = 150
+print(price)
+
+price = 5
+amount = 6
+points = 35
+points = 45
+print(points)
+name = "Tom"
+level = 14
+print(name)
+level = level + 1
+print(level)name = "Tom"
+
+salary = 75000
+print(name)band = "Beatles"
+song = "Yesterday"
+song = "Let it be"
+print(song)
