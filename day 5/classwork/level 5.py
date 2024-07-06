64d7c15a1b0bdfc263966e133c5773979108eeea
gabrieliobashvili <iobashviligabrieli@gmail.com>
2024-07-06 17:57:50 +0400

    second commit

---
+++ day 5/classwork/level 5.py
@@ -0,0 +1,29 @@
+name = "---"
+
+print(name)
+
+
+my name = "---"
+
+print(my name)
+
+
+name = "---"
+print(name)
+
+2
+first_number = 10
+secondnumber = 5
+
+Sum = first_number + second_number
+print(sum)
+
+სახელი = "ჩემსი სახელი"
+
+გვარი = "23"
+
+print(სახელი + int(გვარი))
+
+
+
+
